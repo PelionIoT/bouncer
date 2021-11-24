90ce422aabe3bf8ac51a58a6a02fa6d93f6066bd
Mo Chen <mo.chen@arm.com>
2021-11-24 12:32:51 -0600

    Support multiple simultaneous connections
    Asynchronously accept() connections so that multiple simultaneous proxy
    pairs can exist.  Also add a mechanism to track pairs of connections.  I
    do this by giving a pair of file descriptors to each epoll interest.
    Also added a pointer to the corresponding pair so both can be freed
    together.

--- bouncer.c
+++ bouncer.c
@@ -6,68 +6,151 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-void proxy_loop(int fd0, int fd1)
+struct proxy_pair {
+    int fd_in;  // The socket to read from.  If fd_in == listen_fd, this is the listening socket.
+    int fd_out; // The socket to write to
+    struct proxy_pair *partner; // The other proxy_pair that is related to this one.
+};
+
+int connect_to_docker(char *docker_sock_path);
+
+int add_fd_pair_to_proxy(int epoll_fd, int fd0, int fd1)
 {
-    int event_count, i, rc, other_fd;
-    const int MAX_EVENTS = 1024;
-    struct epoll_event event, events[MAX_EVENTS];
-    char buf[1024];
-
-    int epoll_fd = epoll_create1(0);
-
-    if (epoll_fd == -1) {
-        perror("Failed to create epoll");
-        goto out1;
+    struct epoll_event event;
+    struct proxy_pair *pair1, *pair2;
+    event.events = EPOLLIN;
+    pair1 = (struct proxy_pair *)malloc(sizeof(struct proxy_pair));
+    if (pair1 == NULL) {
+        fprintf(stderr, "Failed to allocate proxy pair\n");
+        return -1;
+    }
+    pair2 = (struct proxy_pair *)malloc(sizeof(struct proxy_pair));
+    if (pair2 == NULL) {
+        fprintf(stderr, "Failed to allocate proxy pair\n");
+        free(pair1);
+        return -1;
     }
 
-    event.events = EPOLLIN;
-    event.data.fd = fd0;
+    pair1->fd_in = fd0;
+    pair1->fd_out = fd1;
+    pair1->partner = pair2;
+    event.data.ptr = pair1;
 
     if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd0, &event)) {
         perror("Failed to add fd to epoll");
-        goto out;
+        return -1;
     }
 
     event.events = EPOLLIN;
-    event.data.fd = fd1;
+    pair2->fd_in = fd1;
+    pair2->fd_out = fd0;
+    pair2->partner = pair1;
+    event.data.ptr = pair2;
 
     if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd1, &event)) {
         perror("Failed to add fd to epoll");
-        goto out;
+        return -1;
+    }
+
+    fprintf(stderr, "Added %d %d to epoll\n", fd0, fd1);
+
+    return 0;
+}
+
+void proxy_loop(int listen_fd, char *docker_sock_path)
+{
+    int epoll_fd, accept_fd, docker_fd, event_count, i, rc, other_fd;
+    const int MAX_EVENTS = 1024;
+    struct epoll_event event, events[MAX_EVENTS];
+    char buf[1024];
+    struct proxy_pair listen_pair, *pair;
+
+    epoll_fd = epoll_create1(0);
+    if (epoll_fd == -1) {
+        perror("Failed to create epoll");
+        goto out2;
+    }
+
+    listen_pair.fd_in = listen_fd;
+    listen_pair.fd_out = -1;
+    event.data.ptr = &listen_pair;
+    event.events = EPOLLIN;
+    rc = epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_fd, &event);
+    if (rc == -1) {
+        perror("Error adding listen socket to epoll");
+        goto out1;
     }
 
     while (1) {
         event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, 30000);
         if (event_count == -1) {
             perror("Failed to wait for epoll events");
-            goto out;
+            break;
         }
         for (i = 0; i < event_count; i++) {
-            rc = read(events[i].data.fd, buf, sizeof(buf));
-            fprintf(stderr, "Read %u bytes from fd %d: %.*s\n", rc, events[i].data.fd, rc, buf);
-            if (rc == -1) {
-                perror("Error while reading from socket");
-                goto out;
-            } else if (rc == 0) {
-                fprintf(stderr, "Got EOF from fd %d.\n", events[i].data.fd);
-                goto out;
+            int done = 0;
+            pair = (struct proxy_pair *)events[i].data.ptr;
+            fprintf(stderr, "got epoll event: %d %d\n", pair->fd_in, pair->fd_out);
+            if (events[i].events & EPOLLHUP) {
+                fprintf(stderr, "epoll HUP\n");
+                done = 1;
+            }
+            if ((events[i].events & EPOLLERR) ||
+                (!(events[i].events & EPOLLIN))) {
+                    fprintf(stderr, "epoll error: %u\n", events[i].events);
+                    goto out1;
+            }
+            if (pair->fd_in == listen_fd) {
+                // Got a new connection
+                if ((accept_fd = accept(listen_fd, NULL, NULL)) == -1) {
+                    perror("Failed to accept connection on proxy socket");
+                    continue;
+                }
+
+                docker_fd = connect_to_docker(docker_sock_path);
+                if (docker_fd == -1) {
+                    fprintf(stderr, "Failed to connect to docker.sock at %s\n", docker_sock_path);
+                    close(accept_fd);
+                    continue;
+                }
+
+                if (add_fd_pair_to_proxy(epoll_fd, accept_fd, docker_fd) == -1) {
+                    goto out1;
+                }
+            } else {
+                rc = read(pair->fd_in, buf, sizeof(buf));
+                fprintf(stderr, "Read %u bytes from fd %d: %.*s\n", rc, pair->fd_in, rc, buf);
+                if (rc == -1) {
+                    perror("Error while reading from socket");
+                    break;
+                } else if (rc == 0) {
+                    fprintf(stderr, "Got EOF from fd %d.\n", pair->fd_in);
+                    done = 1;
+                }
+
+                rc = write(pair->fd_out, buf, rc);
+                if (rc == -1) {
+                    perror("Failed to write to socket");
+                    done = 1;
+                }
+
             }
 
-            other_fd = (events[i].data.fd == fd0 ? fd1 : fd0);
-            rc = write(other_fd, buf, rc);
-            if (rc == -1) {
-                perror("Failed to write to socket");
-                goto out;
+            if (done) {
+                // close()ing a file descriptor makes epoll remove it from the interest list
+                close(pair->fd_in);
+                close(pair->fd_out);
+                free(pair->partner);
+                free(pair);
             }
         }
     }
 
-out:
-    if (close(epoll_fd)) {
-        perror("Failed to close epoll fd");
-    }
 out1:
+    close(epoll_fd);
+out2:
     return;
 }
 
@@ -100,7 +183,7 @@ int connect_to_docker(char *docker_sock_path)
 
 int main(int argc, char **argv)
 {
-    int listen_fd, docker_fd, rc = 0, accept_fd;
+    int listen_fd, rc = 0;
     struct sockaddr_un listen_sockaddr;
 
     if (argc != 3) {
@@ -108,6 +191,7 @@ int main(int argc, char **argv)
         rc = 1;
         goto out3;
     }
+
     listen_fd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (listen_fd == -1) {
         perror("Failed to create listening socket fd\n");
@@ -131,30 +215,12 @@ int main(int argc, char **argv)
     // Accept a connection on the proxy sock
     if (listen(listen_fd, SOMAXCONN) == -1) {
         perror("Failed to listen on proxy socket");
-        goto out;
+        goto out1;
     }
     fprintf(stderr, "Listening at %s\n", listen_sockaddr.sun_path);
 
-    while (1) {
-        if ((accept_fd = accept(listen_fd, NULL, NULL)) == -1) {
-            perror("Failed to accept connection on proxy socket");
-            continue;
-        }
+    proxy_loop(listen_fd, argv[2]);
 
-        docker_fd = connect_to_docker(argv[2]);
-        if (docker_fd == -1) {
-            fprintf(stderr, "Failed to connect to docker.sock at %s\n", argv[2]);
-            close(accept_fd);
-            continue;
-        }
-
-        proxy_loop(accept_fd, docker_fd);
-        close(accept_fd);
-        close(docker_fd);
-    }
-
-out:
-    close(docker_fd);
 out1:
     unlink(listen_sockaddr.sun_path);
 out2:
